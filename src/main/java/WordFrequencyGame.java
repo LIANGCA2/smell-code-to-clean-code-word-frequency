314d6241f2c40049a803233efc5b68766e6f5786
LIANGCA2 <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-13 15:43:16 +0800

    optimize the code

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -9,40 +9,44 @@ public class WordFrequencyGame {
         if (inputStr.split("\\s+").length == 1) {
             return inputStr + " 1";
         } else {
-
-            try {
-
-                //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
-
-                List<Input> inputList = new ArrayList<>();
-                for (String s : arr) {
-                    Input input = new Input(s, 1);
-                    inputList.add(input);
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map = getListMap(inputList);
-
-                List<Input> list = new ArrayList<>();
-                for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
-                    Input input = new Input(entry.getKey(), entry.getValue().size());
-                    list.add(input);
-                }
-                inputList = list;
-
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (Input w : inputList) {
-                    String s = w.getValue() + " " + w.getWordCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-                return "Calculate Error";
-            }
+            List<Input> inputList = groupInputWithKeyword(inputStr);
+            //get the map for the next step of sizing the same word
+            List<Word> resultModel = translateToModel(inputList);
+            return buildResultView(resultModel);
         }
+    }
+
+    public String buildResultView(List<Word> resultModel) {
+        StringJoiner inputView = new StringJoiner("\n");
+        for (Word w : resultModel) {
+            String s = w.getValue() + " " + w.getWordCount();
+            inputView.add(s);
+        }
+        return inputView.toString();
+    }
+
+    public List<Word> translateToModel(List<Input> inputList) {
+        Map<String, List<Input>> map = getListMap(inputList);
+
+        List<Word> resultModel = new ArrayList<>();
+        for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
+            Word word = new Word(entry.getKey(), entry.getValue().size());
+            resultModel.add(word);
+        }
+        resultModel.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+        return resultModel;
+    }
+
+    public List<Input> groupInputWithKeyword(String inputStr) {
+        //split the input string with 1 to n pieces of spaces
+        String[] arr = inputStr.split("\\s+");
+
+        List<Input> inputList = new ArrayList<>();
+        for (String s : arr) {
+            Input input = new Input(s);
+            inputList.add(input);
+        }
+        return inputList;
     }
 
     private Map<String, List<Input>> getListMap(List<Input> inputList) {
